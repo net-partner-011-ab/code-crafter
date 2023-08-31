396d054dd6a25e35b0db781ab6e0139f5f677fcb
slobodanbajic9 <slobodan.bajic9@gmail.com>
2023-08-31 14:11:49 +0200

    Create hero section
    Create hero component based on Bulma CSS framework.

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 'use client'
 import Navbar from '../components/Navbar'
+import Hero from "../components/Hero"
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   )
 }
---
+++ components/Hero.js
@@ -0,0 +1,14 @@
+export default function Hero() {
+  return (
+    <section className="hero is-large is-black">
+      <div className="container">
+        <div className="hero-body">
+          <p className="title pb-4 has-text-centered-mobile">Net Partner 011 Starter Template</p>
+          <p className="subtitle has-text-centered-mobile">
+            A badass template based on Bulma CSS framework.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
