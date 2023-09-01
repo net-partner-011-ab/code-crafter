60601e644f327993e000bd02f8c34971e1bf7b97
slobodanbajic9 <slobodan.bajic9@gmail.com>
2023-09-01 13:13:55 +0200

    Create footer section
    Create footer component based on Bulma CSS classes.

---
+++ components/Footer.js
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <footer className="section has-background-black">
+      <div className="content has-text-centered">
+        <p className="has-text-light">
+          &copy; 2023 | Powered By{" "}
+          <a
+            className="has-text-primary"
+            target="_blank"
+            href="https://np011.se"
+          >
+            Net Partner 011
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+}
